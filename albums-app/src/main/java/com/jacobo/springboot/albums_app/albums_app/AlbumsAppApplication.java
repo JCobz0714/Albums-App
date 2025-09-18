c972a9557788684a072af0cfbf2f6a91e33c4303
J. cob <jacoboosorio299589@gmail.com>
2025-09-18 17:09:44 -0500

    Creating project files

---
+++ albums-app/src/main/java/com/jacobo/springboot/albums_app/albums_app/AlbumsAppApplication.java
@@ -0,0 +1,13 @@
+package com.jacobo.springboot.albums_app.albums_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsAppApplication.class, args);
+	}
+
+}
---
+++ albums-app/src/test/java/com/jacobo/springboot/albums_app/albums_app/AlbumsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobo.springboot.albums_app.albums_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
