fd54ba6ae81d12cccc000035695c18b0fb9b9a85
J. cob <jacoboosorio299589@gmail.com>
2025-09-27 14:35:18 -0500

    Adding hashCode and equals methods to the entities

--- albums-app/src/main/java/entities/Album.java
+++ albums-app/src/main/java/entities/Album.java
@@ -89,4 +89,47 @@ public class Album {
     public void setGenres(List<Genre> genres) {
         this.genres = genres;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+        result = prime * result + ((year == null) ? 0 : year.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Album other = (Album) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (artist == null) {
+            if (other.artist != null)
+                return false;
+        } else if (!artist.equals(other.artist))
+            return false;
+        if (year == null) {
+            if (other.year != null)
+                return false;
+        } else if (!year.equals(other.year))
+            return false;
+        return true;
+    }
 }
--- albums-app/src/main/java/entities/Genre.java
+++ albums-app/src/main/java/entities/Genre.java
@@ -62,4 +62,35 @@ public class Genre {
     public void setAlbums(List<Album> albums) {
         this.albums = albums;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((genreName == null) ? 0 : genreName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Genre other = (Genre) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (genreName == null) {
+            if (other.genreName != null)
+                return false;
+        } else if (!genreName.equals(other.genreName))
+            return false;
+        return true;
+    }
 }
