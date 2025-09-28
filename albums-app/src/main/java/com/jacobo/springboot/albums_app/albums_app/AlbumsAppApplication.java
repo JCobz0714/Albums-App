cea3ba29de24011a709d94b6d69258a9e0fd252f
J. cob <jacoboosorio299589@gmail.com>
2025-09-28 12:32:57 -0500

    Modifying the main file to fix errors

--- albums-app/src/main/java/com/jacobo/springboot/albums_app/albums_app/AlbumsAppApplication.java
+++ albums-app/src/main/java/com/jacobo/springboot/albums_app/albums_app/AlbumsAppApplication.java
@@ -2,9 +2,10 @@ package com.jacobo.springboot.albums_app.albums_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AlbumsAppApplication {
+public class AlbumsAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlbumsAppApplication.class, args);
