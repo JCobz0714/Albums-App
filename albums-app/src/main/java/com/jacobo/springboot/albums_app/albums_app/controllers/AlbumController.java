29796d2d4b29d08941990633859601579d73c2d3
J. cob <jacoboosorio299589@gmail.com>
2025-09-28 12:22:47 -0500

    Commenting what all the methods do

--- albums-app/src/main/java/com/jacobo/springboot/albums_app/albums_app/controllers/AlbumController.java
+++ albums-app/src/main/java/com/jacobo/springboot/albums_app/albums_app/controllers/AlbumController.java
@@ -24,61 +24,79 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
+//Creating the CRUD methods for album entity
 @RestController
 @RequestMapping("/albums")
 public class AlbumController {
+    //Injecting the service as a dependency
     @Autowired
     private AlbumService service;
 
+    //Get method, listing all the albums
     @GetMapping
     public List<Album> list() {
         return service.findAll();
     }
     
+    //Get method for searching a certain album by its ID
     @GetMapping("/{id}")
     public ResponseEntity<?> view(@PathVariable Long id) {
+        //Making the search using the service dependency
         Optional<Album> albumOptional = service.findById(id);
 
+        //Validating if the object was found
         if (albumOptional.isPresent()){
             return ResponseEntity.ok(albumOptional.orElseThrow());
         }
 
+        //Return a not found error
         return ResponseEntity.notFound().build();
     }
 
+    //Post method, creating a new album
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody Album album, BindingResult result) {
+        //Doing validations using validation method created inside ValidationUtil class
         if(result.hasFieldErrors()){
             return ResponseEntity.badRequest().body(ValidationUtil.validation(result));
         }
 
+        //Return a 200 ok response stating that the object was created
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(album));
     }
     
+    //Put method, updating an album
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@Valid @RequestBody Album album, BindingResult result, @PathVariable Long id) {
+        //Doing validations using validation method created inside ValidationUtil class
         if(result.hasFieldErrors()){
             return ResponseEntity.badRequest().body(ValidationUtil.validation(result));
         }
 
+        //Updating the object with the injected service object
         Optional<Album> albumOptional = service.update(id, album);
 
+        //Returning a 200 ok response stating that the object was updated
         if(albumOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.CREATED).body(albumOptional.orElseThrow());
         }
 
+        //Return an error stating that the object was not found
         return ResponseEntity.notFound().build();
     }
 
+    //Delete method
     @DeleteMapping("/{id}")
     public ResponseEntity<Album> delete(@PathVariable Long id){
+        //Using the delete method in the service
         Optional<Album> albumOptional = service.delete(id);
 
+        //Returning an ok response if the object was deleted succesfully
         if(albumOptional.isPresent()){
             return ResponseEntity.ok(albumOptional.orElseThrow());
         }
 
+        //Returning a not found message if the object was not found
         return ResponseEntity.notFound().build();
     }
 }
--- albums-app/src/main/java/com/jacobo/springboot/albums_app/albums_app/controllers/GenreController.java
+++ albums-app/src/main/java/com/jacobo/springboot/albums_app/albums_app/controllers/GenreController.java
@@ -23,60 +23,79 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
+//Creating the CRUD methods for genre entity
 @RestController
 @RequestMapping("/albums/genre")
 public class GenreController {
+    //Injecting the service as a dependency
     @Autowired
     private GenreService service;
     
+    //Get method, listing all the genres
     @GetMapping
     public List<Genre> list() {
         return service.findAll();
     }
     
+    //Get method for searching a certain genre by its ID
     @GetMapping("/{id}")
     public ResponseEntity<?> view(@PathVariable Long id) {
+        //Making the search using the service dependency
         Optional<Genre> genreOptional = service.findById(id);
 
+        //Validating if the object was found
         if (genreOptional.isPresent()){
             return ResponseEntity.ok(genreOptional.orElseThrow());
         }
 
+        //Return a not found error
         return ResponseEntity.notFound().build();
     }
 
+    //Post method, creating a new genre
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody Genre genre, BindingResult result) {
+        //Doing validations using validation method created inside ValidationUtil class
         if(result.hasFieldErrors()){
             return ResponseEntity.badRequest().body(ValidationUtil.validation(result));
         }
 
+        //Return a 200 ok response stating that the object was created
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(genre));
     }
 
+    //Put method, updating a genre
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@Valid @RequestBody Genre genre, BindingResult result, @PathVariable Long id) {
+        //Doing validations using validation method created inside ValidationUtil class
         if(result.hasFieldErrors()){
             return ResponseEntity.badRequest().body(ValidationUtil.validation(result));
         }
 
+        //Updating the object with the injected service object
         Optional<Genre> genreOptional = service.update(id, genre);
 
+        //Returning a 200 ok response stating that the object was updated
         if(genreOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.CREATED).body(genreOptional.orElseThrow());
         }
 
+        //Return an error stating that the object was not found
         return ResponseEntity.notFound().build();
     }
 
+    //Delete method
     @DeleteMapping("/{id}")
     public ResponseEntity<Genre> delete(@PathVariable Long id){
+        //Using the delete method in the service
         Optional<Genre> genreOptional = service.delete(id);
 
+        //Returning an ok response if the object was deleted succesfully
         if(genreOptional.isPresent()){
             return ResponseEntity.ok(genreOptional.orElseThrow());
         }
 
+        //Returning a not found message if the object was not found
         return ResponseEntity.notFound().build();
     }
 }
